eb96a1f6afe64c21d7de0097359950bfb7e2526b
bmolina-nyc <bruce.d.molina@gmail.com>
2016-11-20 23:32:04 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -25,7 +25,11 @@ class SongsController < ApplicationController
   end
 
   def new
-    @song = Song.new
+    if params[:artist_id] && !Artist.exists?(params[:artist_id])
+      redirect_to artists_path, alert: "Artist not found"
+    else
+      @song = Song.new(artist_id: params[:artist_id])
+    end
   end
 
   def create
@@ -38,8 +42,22 @@ class SongsController < ApplicationController
     end
   end
 
+  # What we should do is check to make sure that 1) the author_id is valid and
+  #  2) the post matches the author. 
+  # the point here is the check if a song is in the database
+  # we also need to check that the song is in the db of a valid artist
   def edit
-    @song = Song.find(params[:id])
+    if params[:artist_id]
+      @artist = Artist.find_by(id: params[:artist_id])
+      if @artist.nil? 
+        redirect_to artists_path, alert: "Artist not found." 
+      else # no artist id so its just a /songs/1/edit path for example
+        @song = @artist.songs.find_by(id: params[:id])
+        redirect_to artist_songs_path(@artist), alert: "Song not found." if @song.nil?
+      end
+    else
+      @song = Song.find(params[:id])
+    end
   end
 
   def update
@@ -64,7 +82,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:title, :artist_name)
+    params.require(:song).permit(:title, :artist_name, :artist_id)
   end
 end
 
